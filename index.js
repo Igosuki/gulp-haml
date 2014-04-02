a33334d05f6a6568e2705d4237c41c91bf048203
Guillaume <igosuki@gmail.com>
2014-04-02 16:43:16 +0200

    Allow a layout option, within which 'content' is replaced by the compiled html from the target file

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var map = require('map-stream');
 var rext = require('replace-ext');
+var gutil = require('gulp-util');
 var haml = require('haml');
 
 module.exports = function(options) {
   if(!options) options = {};
   if(!options.ext) options.ext = '.html';
-
   // Map each file to this function
   function hamlStream(file, cb) {
     // Remember that contents is ALWAYS a buffer
@@ -13,6 +13,8 @@ module.exports = function(options) {
     if (file.isStream()) return cb(new Error("gulp-haml: Streaming not supported"));
 
     var html = haml.render(file.contents.toString("utf8"), options);
+    if(options.layout)
+      html = haml.render(new gutil.File({path:options.layout}), {locals: {content:html}});
     file.path = rext(file.path, options.ext);
     file.contents = new Buffer(html);
 
