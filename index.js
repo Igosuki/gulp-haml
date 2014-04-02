9775745a6484624c88b8d42452b2f038796712e3
Guillaume <igosuki@gmail.com>
2014-04-02 16:58:38 +0200

    Only load the layout once

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var gutil = require('gulp-util');
 var haml = require('haml');
 
 module.exports = function(options) {
+  var layoutContent = null;
   if(!options) options = {};
   if(!options.ext) options.ext = '.html';
+  if(options.layout) layoutContent = new gutil.File({path:options.layout}).contents.toString("utf8");
+
   // Map each file to this function
   function hamlStream(file, cb) {
     // Remember that contents is ALWAYS a buffer
@@ -14,7 +17,7 @@ module.exports = function(options) {
 
     var html = haml.render(file.contents.toString("utf8"), options);
     if(options.layout)
-      html = haml.render(new gutil.File({path:options.layout}), {locals: {content:html}});
+      html = haml.render(layoutContent, {locals: {content:html}});
     file.path = rext(file.path, options.ext);
     file.contents = new Buffer(html);
 
